55f0912b325288216e210c3a976568e928ec4ddb
artur <artur@DESKTOP-0KKE3K2>
2023-11-07 22:44:12 +0500

    Добавьте файлы проекта.

---
+++ Artursk/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Ввидите первое чило");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ввидите второе число");
+double b = Convert.ToDouble(Console.ReadLine());
+double result1 = a + b;
+double result2 = a - b;
+double result3 = a * b;
+double result4 = a / b;
+Console.WriteLine($"сумма чисел = {result1} \nразность чисел = {result2} \nумножение чисел = {result3} \nделение чисел = {result4}");
